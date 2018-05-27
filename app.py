52589b24fc066c833c3fa91aec9ef85e8c73a872
Jhonbeltran <jbeltranleon@gmail.com>
2018-05-27 14:29:05 -0500

    Muestro comprobación de aprendizaje e implemento compuerta or

--- app.py
+++ app.py
@@ -30,10 +30,41 @@ def and_gate():
 		prediction = perceptron.predict(np.array([first_input,second_input]))
 		return render_template('predict.html', first_input=first_input,
 											   second_input=second_input,
-											   prediction = prediction)
+											   prediction = prediction,
+											   gate = 'AND',
+											   prev_url='and_gate')
 
 
 	return render_template('and_gate.html', pesos_finales=pesos_finales,
+											last_log=last_log, 
+											learning_lap=learning_lap)
+
+@app.route(r'/or_gate', methods=['GET', 'POST'])
+def or_gate():
+	X = np.array([[0, 0],[0, 1],[1, 0],[1, 1]])
+	d = np.array([0, 1, 1, 1])
+	perceptron = Perceptron(input_size=2) # Creación del esqueleto del perceptrón
+	perceptron.fit(X, d) #Entrenamiento del perceptrón
+	pesos_finales = perceptron.W
+
+	#Encontrar una mejor forma para mostrar los logs
+	log_reverse = perceptron.log[::-1]
+	last_log_reverse = log_reverse[:10]
+	last_log = last_log_reverse[::-1]
+	learning_lap = perceptron.learning_lap
+
+	if request.form:
+		first_input = int(request.form.get('first_input'))
+		second_input = int(request.form.get('second_input'))
+		prediction = perceptron.predict(np.array([first_input,second_input]))
+		return render_template('predict.html', first_input=first_input,
+											   second_input=second_input,
+											   prediction = prediction,
+											   gate = 'OR',
+											   prev_url = '/or_gate')
+
+
+	return render_template('or_gate.html', pesos_finales=pesos_finales,
 											last_log=last_log, 
 											learning_lap=learning_lap)
 
